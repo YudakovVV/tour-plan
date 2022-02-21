97c40786c64a0bc8f448c9627d288a19b0d7b676
YudakovVV <skiff2140@gmail.com>
2022-02-21 22:34:58 +0400

    add footer

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters  
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  }, 
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters  
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  }, 
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function(){
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
+
+$('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpg'});
